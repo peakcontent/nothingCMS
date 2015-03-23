b6011a0d5d8490479a69ffb5caab143c27a521ed
Rick <ricksbrown73@gmail.com>
2015-03-23 21:22:48 +1100

    - Added Grunt build (initially to run JSHint) - Added TravisCI integration

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		jshint: {
+			files: ["*.js"],
+			options: {
+				node: true,  // because the Gruntfile will be linted
+				browser: true,
+				undef: true,
+				unused: true
+			}
+		}
+	});
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.registerTask("default", ["jshint"]);
+};
